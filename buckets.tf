ddd6aaa6858ee985f777cddcfac4b8bb1be90007
Omar Buhidma <omar@buhidma.net>
2023-05-10 19:32:11 -0500

    correcting src and dest,
    and terraform fmt

--- buckets.tf
+++ buckets.tf
@@ -1,18 +1,18 @@
-resource "google_storage_bucket" "transfer-source" {
-  name          = "omar-doit-transfer-source"
+resource "google_storage_bucket" "transfer-dest" {
+  name          = "omar-doit-transfer-dest"
   location      = "US-SOUTH1"
   force_destroy = true
 
   uniform_bucket_level_access = true
-  public_access_prevention = "enforced"
+  public_access_prevention    = "enforced"
 
 }
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "omar-doit-transer-dest"
-  acl    = "private"
+  bucket        = "omar-doit-transer-src"
+  acl           = "private"
   force_destroy = true
 
   control_object_ownership = true
