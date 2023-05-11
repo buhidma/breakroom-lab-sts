4c9d72f979be4601483bb177dadb80a41dc5431a
Omar Buhidma <omar@buhidma.net>
2023-05-10 21:13:52 -0500

    correcting output

--- storage-transfer-service.tf
+++ storage-transfer-service.tf
@@ -3,5 +3,5 @@ data "google_storage_transfer_project_service_account" "sts-transfer" {
 }
 
 output "sts-transfer-account" {
-  value = data.google_storage_transfer_project_service_account.sts-transfer.email
+  value = data.google_storage_transfer_project_service_account.sts-transfer.member
 }
