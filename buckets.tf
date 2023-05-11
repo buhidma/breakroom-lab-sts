0ccdfb4f2a793f4a790df66f463f3ee7df27a1d7
Omar Buhidma <omar@buhidma.net>
2023-05-10 19:16:48 -0500

    completing\ aws provider conf,
    and the base s3 bucket

--- buckets.tf
+++ buckets.tf
@@ -1,9 +1,24 @@
 resource "google_storage_bucket" "transfer-source" {
   name          = "omar-doit-transfer-source"
   location      = "US-SOUTH1"
-  force_destroy = false
+  force_destroy = true
 
   uniform_bucket_level_access = true
   public_access_prevention = "enforced"
 
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "omar-doit-transer-dest"
+  acl    = "private"
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = false
+  }
 }
