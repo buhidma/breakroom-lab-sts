d04c8e0b75b1614208530c1974da2477b09a6a98
Omar Buhidma <omar@buhidma.net>
2023-05-10 21:07:32 -0500

    outputting sts transfer agent account email

--- storage-transfer-service.tf
+++ storage-transfer-service.tf
@@ -1,7 +1,7 @@
-#data "google_storage_transfer_project_service_account" "sts-transfer" {
-#
-#}
+data "google_storage_transfer_project_service_account" "sts-transfer" {
 
-#output "sts-transfer-account" {
-#  value = data.google_storage_transfer_project_service_account.sts-transfer.email
-#}
+}
+
+output "sts-transfer-account" {
+  value = data.google_storage_transfer_project_service_account.sts-transfer.email
+}
