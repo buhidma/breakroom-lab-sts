b72d02955d37700aec986617e46266f21f3183c8
Omar Buhidma <omar@buhidma.net>
2023-05-10 21:29:45 -0500

    output extraction from state
     seems to work

--- backend.tf
+++ backend.tf
@@ -7,3 +7,14 @@ terraform {
     }
   }
 }
+
+data "terraform_remote_state" "buhidma-scalr" {
+  backend = "remote"
+  config = {
+    hostname     = "buhidma.scalr.io"
+    organization = "env-v0ntlps2lneout1l3"
+    workspaces = {
+      name = "buhidma"
+    }
+  }
+}
--- iam.tf
+++ iam.tf
@@ -15,12 +15,12 @@ resource "aws_iam_user_policy_attachment" "sts-transfer-s3-access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-#data "google_iam_policy" "sts-transfer" {
-#    binding {
-#      role = "roles/storage.admin"
-#      members = [ 
-#        "user:"
-#       ]]
-#    }
-#  
-#}
+data "google_iam_policy" "sts-transfer" {
+    binding {
+      role = "roles/storage.admin"
+      members = [ 
+        data.terraform_remote_state.buhidma-scalr.outputs.sts-transfer-account,
+       ]
+    }
+  
+}
